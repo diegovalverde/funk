6554b1986f0317b9e7151dd1f74540cf1ec31ad7
diegovalverde <diego.valverde.g@gmail.com>
2019-06-23 18:19:15 +0200

    fix: dev: fixed behaviour

--- examples/game_of_life/game_of_life.f
+++ examples/game_of_life/game_of_life.f
@@ -1,13 +1,22 @@
 use nth, accumn, s2d, render_board, len, foreach, set_k, map
 
 # Any live cell with fewer than two live neighbours dies
-update_cell( cnt| cnt < 2 ): 0.
+update_cell(alive, cnt |  alive = 1 /\ cnt < 2 ): 0.
 
 # Any live cell with two or three live neighbours lives
-update_cell( cnt | cnt = 2 \/ cnt = 3): 1.
+update_cell(alive, cnt | alive = 1 /\ cnt = 2 ): 1.
+update_cell(alive, cnt | alive = 1 /\ cnt = 3 ): 1.
 
 # Any live cell with more than three live neighbours dies
-update_cell(cnt | cnt > 3 ): 0.
+update_cell(alive, cnt | alive = 1 /\ cnt > 3 ): 0.
+
+# Any dead cell with exactly three live neighbours becomes a live cell
+update_cell(alive, cnt | alive = 0 /\ cnt = 3 ):
+    say(cell,'wake up',cnt)
+    1.
+
+# All others: ie. dead cells with no neighbours remain dead
+update_cell(_, _  ): 0.
 
 update_board(_ , _ , br | len(br) < 5):
         say(len(br), ' =========== STOP ==========')
@@ -20,10 +29,12 @@ update_board(_ <~ [tr], _ <~ [mr], _ <~ [br] ):
 
 
     cnt <- accumn(tr,  3) + accumn(br, 3) + mr + nth(mr, 2)
-    say('cnt = ', cnt)
+    # change syntax to say('cell',[mr],'cnt =',cnt)
+
+    say('cell',mr,'cnt =',cnt)
 
 
-    update_cell(cnt) ~> [update_board( tr, mr, br  )].
+    update_cell(mr, cnt) ~> [update_board( tr, mr, br  )].
 
 
 s2d_render():
@@ -33,7 +44,7 @@ s2d_render():
 
     new_board <- update_board(board, nth(board,w), nth(board,2*w))
 
-    
+
 
     render_board(new_board, 150, 100, w, 10 ).
 
