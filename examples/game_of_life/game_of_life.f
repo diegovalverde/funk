dbf01a69de5b956774e128f4278020687107fb13
diegovalverde <diego.valverde.g@gmail.com>
2019-06-25 23:52:46 +0200

    [fix][usr] fixing algo @wip

--- examples/game_of_life/game_of_life.f
+++ examples/game_of_life/game_of_life.f
@@ -4,11 +4,15 @@ use nth, accumn, s2d, render_board, len, foreach, set_k, map
 update_cell(alive, cnt |  alive = 1 /\ cnt < 2 ): 0.
 
 # Any live cell with two or three live neighbours lives
-update_cell(alive, cnt | alive = 1 /\ cnt = 2 ): 1.
+update_cell(alive, cnt | alive = 1 /\ cnt = 2 ):
+    say('keep')
+    1.
 update_cell(alive, cnt | alive = 1 /\ cnt = 3 ): 1.
 
 # Any live cell with more than three live neighbours dies
-update_cell(alive, cnt | alive = 1 /\ cnt > 3 ): 0.
+update_cell(alive, cnt | alive = 1 /\ cnt > 3 ):
+    say('die')
+    0.
 
 # Any dead cell with exactly three live neighbours becomes a live cell
 update_cell(alive, cnt | alive = 0 /\ cnt = 3 ):
@@ -22,11 +26,20 @@ update_board(_ , _ , br, i | len(br) < 5):
         say(len(br), ' =========== STOP ==========')
         [].
 
-update_board(_ <~ [tr], _ <~ [mr], _ <~ [br], i ):
-    cnt <- accumn(tr,  3) + accumn(br, 3) + mr + nth(mr, 2)
+
+update_board(a <~ [tr], b <~ [mr], c <~ [br], i ):
+    cnt <- a + b + c  + br + tr + nth(tr,1) + nth(mr, 1) + nth(br,1)
+    #TODO accumn has a bug
+    #say('>>>>',a,b,c,accumn(tr,  2) , accumn(br, 2) , nth(mr, 1))
+
+    say(a, tr, nth(tr, 1))
+    say(b, mr, nth(mr, 1))
+    say(c, br, nth(br, 1))
+
     # change syntax to say('cell',[mr],'cnt =',cnt)
 
     say(i, 'cell',mr,'cnt =',cnt)
+    say('')
 
 
     update_cell(mr, cnt) ~> [update_board( tr, mr, br, i + 1  )].
@@ -35,21 +48,24 @@ update_board(_ <~ [tr], _ <~ [mr], _ <~ [br], i ):
 s2d_render():
     w <- 50
 
-    board <-  set_k(set_k(set_k(set_k([0 | 0 <= cell < 2000 ], 210, 1),211,1),212,1),50,666)
+    board <-  set_k(set_k(set_k([0 | 0 <= cell < 2000 ], 210, 1),211,1),212,1)
 
     #board <-  set_k(set_k(set_k([cell | 0 <= cell < 2000 ], 2, 77),3,78),4,79)
     # say(board)
     say( '50 ->',nth(board, 50))
-    # say( '211 ->',nth(board, 211))
+    say( '50 ->',nth(board, w))
+    say( '200 ->',nth(board, 200))
     # say( '212 ->',nth(board, 212))
+    mr <- nth(board, w)
+
 
     new_board <- update_board(board, nth(board,w), nth(board,2*w), w)
 
 
 
-    render_board(new_board, 150, 100, w, 10 ).
+    render_board(board, 150, 100, w, 10 ).
 
 
 main():
-    funk_set_config(0,3)
+    funk_set_config(0,1)
     s2d_window('the game of life', 800, 600 ).
