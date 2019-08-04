b9510b3745419e233ea1af87c4a4e64c805689fb
diegovalverde <diego.valverde.g@gmail.com>
2019-08-04 16:47:08 +0200

    @cosmetic

--- examples/game_of_life/game_of_life.f
+++ examples/game_of_life/game_of_life.f
@@ -4,19 +4,13 @@ use nth, idem, accumn, s2d, render_board, len, foreach, set_k, map
 update_cell(alive, cnt |  alive = 1 /\ cnt < 2 ): 0.
 
 # Any live cell with two or three live neighbours lives
-update_cell(alive, cnt | alive = 1 /\ cnt = 2 ):
-    say('keep')
-    1.
+update_cell(alive, cnt | alive = 1 /\ cnt = 2 ): 1.
 update_cell(alive, cnt | alive = 1 /\ cnt = 3 ): 1.
 
 # Any live cell with more than three live neighbours dies
-update_cell(alive, cnt | alive = 1 /\ cnt > 3 ):
-    say('die')
-    0.
+update_cell(alive, cnt | alive = 1 /\ cnt > 3 ): 0.
 # Any dead cell with exactly three live neighbours becomes a live cell
-update_cell(alive, cnt | alive = 0 /\ cnt = 3 ):
-    say(cell,'wake up',cnt)
-    1 .
+update_cell(alive, cnt | alive = 0 /\ cnt = 3 ): 1.
 
 # All others: ie. dead cells with no neighbours remain dead
 update_cell(_, _  ): 0.
@@ -41,58 +35,21 @@ ub(tr, mr, br,k, i,j,w| i = (w-1)):
 
 
 ub(a <~ [tr], b <~ [mr], c <~ [br],k,i,j,w):
-    say(k,'>>>>>>>',i,j)
     cnt <- a + b + c + br + tr + nth(tr,1) + nth(mr, 1) + nth(br,1)
     update_cell(mr, cnt) ~> [ub( tr, mr, br, k+1,(k+1)/w,(k+1)%w,w )].
 
-
-
-update_board(a <~ [tr], b <~ [mr], br | len(br) = 1):
-    [br].
-
-update_board(_ , _ , [],_ ): [].
-
-update_board(a <~ [tr], b <~ [mr], c <~ [br] | len(br) = 1 ):
-    update_cell(mr, 0) ~> [update_board( tr, mr, br )].
-
-update_board(a <~ [tr], b <~ [mr], c <~ [br]) :
-    cnt <- a + b + c + br + tr + nth(tr,1) + nth(mr, 1) + nth(br,1)
-    update_cell(mr, cnt) ~> [update_board( tr, mr, br )].
-
-layer(_,[],_,_,_,_): [].
-
-layer(fg,b <~ [bg],k,i,j,w | len(fg) = 0):
-    say(len(fg),len(bg),'XXX k',k,i,j)
-    idem(b) ~> [layer(fg,bg,k+1,(k+1)%w,(k+1)/w,w)].
-
-layer(fg,b<~[bg],k, i,j,w| i = (w-1) \/ j = (w-1)):
-    say(len(fg),len(bg),'BG2 k',k,i,j)
-    idem(b) ~>[layer(fg,bg,k+1,(k+1)%w,(k+1)/w,w)].
-
-
-layer(fg,b<~[bg],k, i,j,w| i = 0 \/ j = 0):
-    say(len(fg),len(bg),'BG k',k,i,j)
-    idem(b) ~>[layer(fg,bg,k+1,(k+1)%w,(k+1)/w,w)].
-
-layer(f<~[fg],b<~[bg],k,i,j,w):
-    say(len(fg),len(bg),'FG k',k,i,j)
-    idem(f) ~>[layer(fg,bg,k+1,(k+1)%w,(k+1)/w,w)].
-
-
 s2d_render(board):
     w <- 8
+    h <- 5
+    next_board <- ub(board, nth(board,w), nth(board,2*w), 0, 0,0,w)
+    render_board(next_board, 10, 10, w, 80 )
 
-    #inner_board <- update_board(board, nth(board,w), nth(board,2*w))
-    #next_board <- layer(inner_board,[1 | 0 <= cell < 40 ],0,0,0,w)
-
-    #render_board(board, 10, 10, 8, 80 )
-    da_b <- ub(board, nth(board,w), nth(board,2*w), 0, 0,0,w)
-    render_board(da_b, 10, 10, 8, 80 )
-
-    #render_board(inner_board, 10, 10, 6, 80 )
-    s2d_render(da_b).
+    s2d_render(next_board).
 
 main():
+    w <- 8
+    h <- 5
+
     funk_set_config(0,1)
     funk_set_config(1,10)
 
