934a5cf6dc9449c7e4e149a1c7ecf69ba178662d
diegovalverde <diego.valverde.g@gmail.com>
2020-12-25 19:34:41 +0100

    prevented SLD from stealing keyboard input in html5

--- funk/core/c_model/funk_sdl.c
+++ funk/core/c_model/funk_sdl.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #if FUNK_BUILD_FOR_WEB
 #include <emscripten.h>
+#include <emscripten/html5.h>
 #endif
 
 #include "funk_sdl.h"
@@ -60,7 +61,15 @@ void sdl_render_loop(void *arg)
 
 }
 
-
+// The event handler functions can return 1 to suppress the event and disable the default action. That calls event.preventDefault();
+// Returning 0 signals that the event was not consumed by the code, and will allow the event to pass on and bubble up normally.
+EM_BOOL key_callback(int eventType, const EmscriptenKeyboardEvent *e, void *userData)
+{
+  printf("In Weeeeeeeeeee\n");
+  // Take a lool at
+  //https://chromium.googlesource.com/external/github.com/kripken/emscripten/+/refs/tags/1.36.0/tests/test_html5_fullscreen.c
+  return 0;
+}
 
 void funk_sdl_create_window(int32_t w, int32_t h, struct tnode * user_data)
 {
@@ -72,6 +81,11 @@ void funk_sdl_create_window(int32_t w, int32_t h, struct tnode * user_data)
     SDL_Window *window;
     //SDL_Renderer *renderer;
 
+    emscripten_set_keypress_callback("#canvas", 0, 0, key_callback);
+    //Emscripten, by default, captures all user events to the page.
+    //This makes sense for a fullscreen game but not for out application
+
+    SDL_SetHint(SDL_HINT_EMSCRIPTEN_KEYBOARD_ELEMENT, "#canvas");
     if (SDL_CreateWindowAndRenderer(w, h, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
 
@@ -83,17 +97,22 @@ void funk_sdl_create_window(int32_t w, int32_t h, struct tnode * user_data)
 
 
   #if FUNK_BUILD_FOR_WEB
+
     // This is for when you want to use emscripten to
     // compile into webassembly and run in the browser
     int simulate_infinite_loop = 1; // call the function repeatedly
-    int fps = -1; // call the function as fast as the browser wants to render (typically 60fps)
+    int fps = 1; // call refresh every second, otherwise the browser wants to render at 60fps
+
+
     emscripten_set_main_loop_arg(sdl_render_loop, &ctx, fps, simulate_infinite_loop);
+
+
   #else
     // This is the path to run on your laptop
     SDL_Event event;
     while (1){
       sdl_render_loop((void*)&ctx);
-      
+
       SDL_PollEvent(&event);
       if (event.type == SDL_QUIT) {
             break;
