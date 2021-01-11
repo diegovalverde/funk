2fe6686fae069ca3d6ce5c265a87341beedd6cfb
diegovalverde <diego.valverde.g@gmail.com>
2021-01-11 19:51:21 +0100

    Adding addtional tests

--- tests/test.f
+++ tests/test.f
@@ -201,11 +201,14 @@ main():
 
       #test sort
       assert(arr_eq([1,2,3], [1,2,3]), 1)
+      assert(arr_eq([1,2,3,4], [1,2,3,4]), 1)
       assert(arr_eq([1,2,3], [1,2,3,4]), 0)
       assert(arr_eq([1,2,3], [5,2,3]), 0)
+      assert(arr_eq([10,100], sort([100,10]) ), 1)
       assert(arr_eq(sort([3,2,1]), [1,2,3]), 1)
       assert(arr_eq(sort([2, 5, 4, 3, 1]), [1,2,3,4,5]), 1)
 
       assert(arr_eq(sort([12,8,2,9,15,4,13,0,6,11,7,1,14,5,3,10]), [i | 0 <= i <= 15]), 1)
+      assert(arr_eq(sort([98,95,92,90,93,96,91,99,94,97]), [i | 90 <= i <= 99]), 1)
 
       say('All tests passed ;)').
