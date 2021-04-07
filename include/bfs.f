1b20a194b094dfa8216c34c023e4f56ce62e43b4
diego.valverde <diego.valverde.g@gmail.com>
2021-04-07 18:21:09 +0200

    chg: bfs

--- include/bfs.f
+++ include/bfs.f
@@ -4,9 +4,11 @@
 bfs(_, _, []):[].
 
 # found a solution
+
+
 bfs(S, N, explore | S(explore[0]) = 1):
     explore[0].
 
 # add children to search in BFS fashion
 bfs(S, N,  n <~ [explore]):
-    bfs( S, N, [explore] <~ N(n) ).
+    bfs( S, N, [explore] ++ [N(n)] ).
