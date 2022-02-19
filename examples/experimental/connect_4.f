bed498aaafcfe067a95f539884e736414ff3e6fa
diego.valverde <diego.valverde.g@gmail.com>
2022-02-19 18:11:36 +0100

    added sudoku test

--- examples/experimental/connect_4.f
+++ examples/experimental/connect_4.f
@@ -1,23 +1,34 @@
+use any, all, arg_eq, min, max, find
+H <-> 6
+W <-> 7
+# Allis (1988)
+VALUES <- [[3,4,5,7,5,4,3], [4,6,8,9,8,6,4],[5,8,11,13,11,8,5],[5,8,11,13,11,8,5],[4,6,8,9,8,6,4],[3,4,5,7,5,4,3]]
+
+heuristic(M,player): sum(VALUES * player * M).
+
 check_4x4(M, player):
-    # diagonal / antidiagonal
     any(all([ S[i,i] | 0 <= i <= 3 ],  player),
         all([ S[3-i,i] | 0 <= i <=3 ], player),
-    # rows / cols
         all(S[i, 0..3]) | 0 <= i <= 3],player),
         all(S[0..3, j]) | 0 <= j <= 3 ],player)).
 
 # Slinding Window
 is_win(M, player):
-    any([[check_4x4(M[i .. i+4 , j..j+4], player) | 
-            0 <= j < -4] | 0 <= i <= -4 ], 1).
+    any([[check_4x4(M[i .. i+4 , j..j+4], player) |  0 <= j < W] | 0 <= i <= H ], 1).
 
-# return columns with at least a zero
+# return index of columns with at least a zero
 get_valid_cols(M): arg_eq([find(0, M[0:H, j]) | 0 <= j < W ], 1).
+
+ 
+move(M, col, player): 
+    l <- M[0..H, 0..col-1]
+    m <- M[0..H, col] 
+    find(0, H - reverse(flatten(m))
+    [m] ++ M[0..H, col+1..W].
 
 minimax(M, _, player | is_win(maximizingPlayer)): 1000.
 minimax(M, _, player | is_win(minimizingPlayer)): -1000.
-
-minimax(M, depth, player | depth=0): heuristic(M,player).
+minimax(M, 0, player): heuristic(M,player).
 
 minimax(M, depth, player  | player = maximizingPlayer):
         max([ minimax( move(M, col, player), depth-1, minimizingPlayer)) |
--- examples/experimental/sudoku/sudoku_backtracking.f
+++ examples/experimental/sudoku/sudoku_backtracking.f
@@ -1,40 +1,19 @@
-use find, unique, fread_list
-
-
-do_until_success(_,[1,B]): [1,B].
-do_until_success([],_): [0,[]].
-do_until_success( f <~ [functions], [0,_]) : 
-     func <- f[0]
-     done <- func(f[1])
-     do_until_success(functions, done).
-
-replace(M, p, q, v | p = q ): v.
-replace(M, _, [i,j], _ ): M[i,j].
-
-m_replace_element(M, v, pos):
-   [ [ replace(M,pos,[i,j], v) | 0 <= j < len(M[0])] | 0 <= i < len(M) ].
+use find, unique, do_until_success, replace_matrix_element
 
 is_valid(B, v, [i,j]):
     x <- [B[i]] ++ [B[0..-1, j]]
     -1* find(v,unique([x] ++ [flatten(B[3*(i/3)  .. 3*(i/3)+2, 3*(j/3) .. 3*(j/3)+2])])).
 
-
 get_next_empty_position(B): 
     p <- find(0,flatten(B))
     [p / 9, p % 9].
 
-solve_sudoku(B | find(0,flatten(B)) = -1): [1,B]. 
-solve_sudoku(B):
+sudoku_backtracking(B | find(0,flatten(B)) = -1): [1,B]. 
+sudoku_backtracking(B):
     pos <- get_next_empty_position(B)
-    do_until_success([ [s1, [B,v,pos]  ] | 1 <= v <=  9],[0,[]]).
+    do_until_success([ [solve, [B,v,pos]  ] | 1 <= v <=  9]).
 
-s1([B, v, pos] | is_valid(B,v,pos) != 1): [0,[]].
-s1([B, v, pos]): 
-    M <- m_replace_element(B, v, pos)
-    solve_sudoku(M).
-    
-main():
-    B <- reshape(fread_list('/Users/diego/workspace/projects/funky_programming_language/examples/experimental/sudoku/puzzle1.txt'),9,9)
-    solution <- solve_sudoku(B) 
-    say('solution\n',solution)
-    1.
+solve([B, v, pos] | is_valid(B,v,pos) != 1): [0,[]].
+solve([B, v, pos]): 
+    M <- replace_matrix_element(B, v, pos)
+    sudoku_backtracking(M).
