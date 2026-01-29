16a0f569648780260cb603e28f2805114b761ca2
Diego <diego.valverde.g@gmail.com>
2026-01-29 14:33:49 +0100

    Maintenance 2026 - 6

--- examples/fizzbuzz/fizzbuzz.f
+++ examples/fizzbuzz/fizzbuzz.f
@@ -3,10 +3,18 @@
 #
 use foreach
 
-fbz(x | x % 15 = 0): say(x, ' FizzBuzz') .
-fbz(x | x % 3 = 0):  say(x, ' Fizz') .
-fbz(x | x % 5 = 0):  say(x, ' Buzz') .
-fbz(x) : say(x).
+fbz(x | x % 15 = 0):
+    say(x, ' FizzBuzz')
+    1.
+fbz(x | x % 3 = 0):
+    say(x, ' Fizz')
+    1.
+fbz(x | x % 5 = 0):
+    say(x, ' Buzz')
+    1.
+fbz(x):
+    say(x)
+    1.
 
 main():
     foreach( [x | 1 <= x <= 100] ,  fbz ).
